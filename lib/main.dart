ae30dd33e7bd2c61eb545a58143e318983d8021a
Pankaj Jangid <pankaj0619@gmail.com>
2023-07-18 20:01:07 +0530

    Communication between 2 cubits using listener

--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider<ColorCubit>(create: (context) => ColorCubit(),),
-        BlocProvider<CounterCubit>(create: (context) => CounterCubit(context.read<ColorCubit>()),)
+        BlocProvider<ColorCubit>(
+          create: (context) => ColorCubit(),
+        ),
+        BlocProvider<CounterCubit>(
+          create: (context) => CounterCubit(),
+        )
       ],
       child: MaterialApp(
         title: 'Bloc Cubits Communication',
@@ -31,39 +35,70 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int incrementSize = 1;
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: context.watch<ColorCubit>().state.color,
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("Bloc Cubits Communication"),
-      ),
-      body:Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: (){
-                context.read<ColorCubit>().changeColor();
-              },
-              child: const Text(
-                'Change Color',style: TextStyle(fontSize: 24),
+    return BlocListener<ColorCubit, ColorState>(
+      listener: (context, state) {
+        if (state.color == Colors.red) {
+          incrementSize = 1;
+        } else if (state.color == Colors.green) {
+          incrementSize = 10;
+        } else if (state.color == Colors.blue) {
+          incrementSize = 100;
+        } else if (state.color == Colors.black) {
+          context.read<CounterCubit>().updateCounter(incrementSize);
+          incrementSize = -100;
+        }
+      },
+      child: Scaffold(
+        backgroundColor: context.watch<ColorCubit>().state.color,
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text("Bloc Cubits Communication"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: () {
+                  context.read<ColorCubit>().changeColor();
+                },
+                child: const Text(
+                  'Change Color',
+                  style: TextStyle(fontSize: 24),
+                ),
               ),
-            ),
-            SizedBox(height: 20,),
-            Text(
-              '${context.watch<CounterCubit>().state.counter}',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            SizedBox(height: 20,),
-            ElevatedButton(onPressed: (){
-              context.read<CounterCubit>().updateCounter();
-            }, child: Text("Increment counter",style: TextStyle(fontSize: 24),))
-          ],
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                '${context.watch<CounterCubit>().state.counter}',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    context.read<CounterCubit>().updateCounter(incrementSize);
+                  },
+                  child: Text(
+                    "Increment counter",
+                    style: TextStyle(fontSize: 24),
+                  ))
+            ],
+          ),
         ),
       ),
     );
